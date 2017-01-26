413a7ee02d222f2f3438206116ef67753643dde1
Babatunde Adeyemi <barbietunnie@gmail.com>
2017-01-26 16:29:32 +0100

    Final version

--- src/pages/stop-timing/stop-timing.ts
+++ src/pages/stop-timing/stop-timing.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import { NavController, NavParams, ViewController } from 'ionic-angular';
 
 /*
   Generated class for the StopTiming page.
@@ -12,11 +12,25 @@ import { NavController, NavParams } from 'ionic-angular';
   templateUrl: 'stop-timing.html'
 })
 export class StopTimingPage {
+  hours: number;
+  minutes: number;
+  seconds: number;
+  elapsedTime: any = 0;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {}
+  constructor(public navCtrl: NavController, public navParams: NavParams, 
+                                            public viewCtrl: ViewController) {}
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad StopTimingPage');
+    this.elapsedTime = this.navParams.get('elapsedTime');
+
+    this.hours = Math.floor(this.elapsedTime / 60);
+    this.minutes = Math.floor(this.minutes / 60);
+    this.seconds = Math.floor(this.elapsedTime % 60);
+  }
+
+  submitTime(): void {
+    let modifiedSeconds = (this.hours * 60 * 60) + (this.minutes * 60) + this.seconds;
+    this.viewCtrl.dismiss(modifiedSeconds);
   }
 
 }
